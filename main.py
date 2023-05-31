bd93363d5279125f455e2aaffaa2690fbca1370b
Maarten Wolfsen <maartenwolfsen@gmail.com>
2023-05-31 16:14:43 +0200

    Add moving cube

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ display_width = 800
 display_height = 600
 pygame.display.set_mode((display_width, display_height), DOUBLEBUF | OPENGL)
 
-# Set up the perspective
-gluPerspective(45, (display_width / display_height), 0.1, 50.0)
-
 # Set up the initial position of the player
 player_x = 0.0
 player_y = 0.0
@@ -24,25 +21,72 @@ player_speed = 0.1
 
 # Set the initial viewport and initialize the model view matrix
 glViewport(0, 0, display_width, display_height)
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluPerspective(45, (display_width / display_height), 0.1, 50.0)
 glMatrixMode(GL_MODELVIEW)
 glLoadIdentity()
 
-# Enable lighting and set up a light source
+# Enable lighting and set up light sources
 glEnable(GL_LIGHTING)
 glEnable(GL_LIGHT0)
 glLightfv(GL_LIGHT0, GL_POSITION, [1.0, 1.0, 1.0, 0.0])
-glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5, 1.0])
+glLightfv(GL_LIGHT0, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
 glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1.0])
 glLightfv(GL_LIGHT0, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
 
 # Enable depth testing
 glEnable(GL_DEPTH_TEST)
 
+# Set up the material properties of the cube
+glMaterialfv(GL_FRONT, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
+glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.8, 0.8, 0.8, 1.0])
+glMaterialfv(GL_FRONT, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+glMaterialfv(GL_FRONT, GL_SHININESS, 50.0)
+
+
+# Define the cube drawing function
+def draw_cube(size):
+    vertices = [
+        [size / 2, -size / 2, -size / 2],
+        [size / 2, -size / 2, size / 2],
+        [-size / 2, -size / 2, size / 2],
+        [-size / 2, -size / 2, -size / 2],
+        [size / 2, size / 2, -size / 2],
+        [size / 2, size / 2, size / 2],
+        [-size / 2, size / 2, size / 2],
+        [-size / 2, size / 2, -size / 2]
+    ]
+
+    faces = [
+        [0, 1, 2, 3],
+        [3, 2, 6, 7],
+        [7, 6, 5, 4],
+        [4, 5, 1, 0],
+        [5, 6, 2, 1],
+        [7, 4, 0, 3]
+    ]
+
+    normals = [
+        [0.0, -1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, -1.0],
+        [1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0]
+    ]
+
+    glBegin(GL_QUADS)
+    for face in faces:
+        normal = normals[faces.index(face)]
+        glNormal3fv(normal)
+        for vertex in face:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+
 # Main game loop
 while True:
-    # Clear the screen and depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -60,44 +104,18 @@ while True:
     if keys[pygame.K_s]:
         player_z += player_speed
 
-    # Limit the player's position to within the screen boundaries
-    if player_x < -display_width / 2:
-        player_x = -display_width / 2
-    elif player_x > display_width / 2:
-        player_x = display_width / 2
+    # Clear the screen and depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     # Set up the player's view
     glLoadIdentity()
-    gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0)
+    gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
-    # Calculate the distance from the light source to the cube
-    distance_to_light = abs(player_z) + 0.1  # Adjust the parameters to control the transition
-
-    # Calculate the interpolated color
-    start_color = [1.0, 0.0, 0.0]  # Red
-    end_color = [0.0, 0.0, 0.0]  # Black
-    interpolated_color = [start + (end - start) * distance_to_light for start, end in zip(start_color, end_color)]
-
-    # Set up the material properties
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, interpolated_color)
-    glMaterialfv(GL_FRONT, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
-    glMaterialfv(GL_FRONT, GL_SHININESS, 50.0)
-
-    # Render the player cube
-    cube_size = 0.5 + abs(player_z) * 0.01  # Adjust the parameters to control the scaling
-    half_width = cube_size / 2.0
+    # Render the cube
+    glPushMatrix()
     glTranslatef(player_x, player_y, player_z)
-    glScalef(cube_size, cube_size, cube_size)  # Scale the cube based on z-value
-    glBegin(GL_QUADS)
-    # Front face
-    glNormal3f(0.0, 0.0, 1.0)
-    glVertex3f(-half_width, -half_width, half_width)
-    glVertex3f(half_width, -half_width, half_width)
-    glVertex3f(half_width, half_width, half_width)
-    glVertex3f(-half_width, half_width, half_width)
-    glEnd()
-    glScalef(1.0 / cube_size, 1.0 / cube_size, 1.0 / cube_size)  # Restore the scale
-    glTranslatef(-player_x, -player_y, -player_z)
+    draw_cube(0.5)
+    glPopMatrix()
 
     # Update the display
     pygame.display.flip()
