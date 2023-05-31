8115d82cff23694692d7d3ade44429d129283797
Maarten Wolfsen <maartenwolfsen@gmail.com>
2023-05-31 22:30:45 +0200

    Add first person camera

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# Initialize Pygame
 pygame.init()
 
-# Set up the display
 display_width = 800
 display_height = 600
 pygame.display.set_mode((display_width, display_height), DOUBLEBUF | OPENGL)
 
-# Set up the initial position of the player
 player_x = 0.0
 player_y = 0.0
 player_z = 0.0
 
-# Set up the initial movement speed of the player
 player_speed = 0.1
+player_rotation_speed = 0.5  # Adjust the sensitivity here
+mouse_sensitivity = 0.01  # Adjust the mouse sensitivity here
 
-# Set the initial viewport and initialize the model view matrix
+camera_yaw = 0.0
+camera_pitch = 0.0
+
 glViewport(0, 0, display_width, display_height)
 glMatrixMode(GL_PROJECTION)
 glLoadIdentity()
@@ -27,26 +27,9 @@ gluPerspective(45, (display_width / display_height), 0.1, 50.0)
 glMatrixMode(GL_MODELVIEW)
 glLoadIdentity()
 
-# Enable lighting and set up light sources
-glEnable(GL_LIGHTING)
-glEnable(GL_LIGHT0)
-glLightfv(GL_LIGHT0, GL_POSITION, [1.0, 1.0, 1.0, 0.0])
-glLightfv(GL_LIGHT0, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
-glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1.0])
-glLightfv(GL_LIGHT0, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
-
-# Enable depth testing
 glEnable(GL_DEPTH_TEST)
 
-# Set up the material properties of the cube
-glMaterialfv(GL_FRONT, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
-glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.8, 0.8, 0.8, 1.0])
-glMaterialfv(GL_FRONT, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
-glMaterialfv(GL_FRONT, GL_SHININESS, 50.0)
-
-
-# Define the cube drawing function
-def draw_cube(size):
+def draw_cube(size, position):
     vertices = [
         [size / 2, -size / 2, -size / 2],
         [size / 2, -size / 2, size / 2],
@@ -81,19 +64,17 @@ def draw_cube(size):
         normal = normals[faces.index(face)]
         glNormal3fv(normal)
         for vertex in face:
-            glVertex3fv(vertices[vertex])
+            glVertex3fv([vertices[vertex][0] + position[0], vertices[vertex][1] + position[1], vertices[vertex][2] + position[2]])
     glEnd()
 
+def handle_input():
+    global player_x, player_z, camera_yaw, camera_pitch
 
-# Main game loop
-while True:
-    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    # Get the state of the keyboard
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         player_x -= player_speed
@@ -104,19 +85,32 @@ while True:
     if keys[pygame.K_s]:
         player_z += player_speed
 
-    # Clear the screen and depth buffer
+    mouse_position = pygame.mouse.get_pos()
+    screen_center = (display_width // 2, display_height // 2)
+    mouse_movement = (mouse_position[0] - screen_center[0], mouse_position[1] - screen_center[1])
+
+    pygame.mouse.set_pos(screen_center)  # Reset mouse position to the center of the screen
+
+    camera_yaw += mouse_movement[0] * mouse_sensitivity  # Horizontal looking
+    camera_pitch -= mouse_movement[1] * mouse_sensitivity  # Vertical looking
+
+def update_camera():
+    glLoadIdentity()
+    glRotatef(-camera_pitch, 1, 0, 0)
+    glRotatef(camera_yaw, 0, 1, 0)
+    glTranslatef(-player_x, -player_y, -player_z)
+
+def render_scene():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # Set up the player's view
-    glLoadIdentity()
-    gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-
-    # Render the cube
     glPushMatrix()
-    glTranslatef(player_x, player_y, player_z)
-    draw_cube(0.5)
+    draw_cube(0.5, (0, 0, 0))
     glPopMatrix()
 
-    # Update the display
     pygame.display.flip()
     pygame.time.wait(10)
+
+while True:
+    handle_input()
+    update_camera()
+    render_scene()
